5b7d8d8ad7f9ac27267719019960a528ca55295f
FastMinion <41300258+FastMinion@users.noreply.github.com>
2021-08-11 20:02:37 +0200

    Add files via upload
    Config,Database

---
+++ system/config.php
@@ -0,0 +1,8 @@
+<?php
+
+class Config{
+    static $dbuser = "Test";
+    static $dbpswd = "Test";
+    static $dbname = "Test";
+    static $dbprefix = "";
+}
---
+++ system/database.php
@@ -0,0 +1,10 @@
+<?php
+
+class Database{
+    private string $dbuser = Config::$dbuser;
+    private string $dbpswd;
+    private string $dbname;
+    private string $dbprefix;
+    
+
+}
